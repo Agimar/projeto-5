3c5fc6ba576a32b702f7736609b4c9b2137f4d2a
agimar <agimar@outlook.com>
2023-05-23 13:17:03 -0300

    comit-3

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
     return (
             <div>
                 <h1>Home</h1>
-                <p>vercel.com/dashboard></p>
+                <p>vercel.com/dashboard</p>
 
                 <Contador/>
             </div>
